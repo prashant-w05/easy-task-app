22496a16fb1843127805a071645fd0dffe27465b
prashant <prashantwaghmode05@gmail.com>
2025-01-22 10:45:31 +0530

    task management app using angular

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    styleUrl: './header.component.css',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {}
