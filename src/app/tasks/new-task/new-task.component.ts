eb8521ed152f141aa8477c63f7d42cc4db6f780d
prashant <prashantwaghmode05@gmail.com>
2025-01-24 16:20:36 +0530

    added task service and local storage for saving task in brwoser

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -8,22 +9,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<{title: string, summary: string, dueDate: string}>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  
   enteredTitle = '';
   enteredSummary = ''
   enteredDate = ''
 
+  private tasksService = inject(TasksService);
+
   onCancel() {
-      this.cancel.emit();
+      this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
-      title:this.enteredTitle,
-      summary: this.enteredSummary,
-      dueDate: this.enteredDate
-    })
+      this.tasksService.addTask({
+        title:this.enteredTitle,
+        summary: this.enteredSummary,
+        dueDate: this.enteredDate
+      }, this.userId);
+      this.close.emit();
+
   }
 
 }
